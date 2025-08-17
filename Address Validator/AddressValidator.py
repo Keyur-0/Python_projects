78c5398850ed31376834d61b8ca1d4dcca81d206
Keyur-0 <keyurkhnat001@gmail.com>
2025-08-17 19:43:54 +0530

    Fixing and updating the logic

--- Address Validator/AddressValidator.py
+++ Address Validator/AddressValidator.py
@@ -1,16 +1,25 @@
 def addressVal(address):
+    if "@" not in address or "." not in address:
+        return False
+    
     dot = address.find(".")
     at = address.find("@")
-    if (dot == -1):
-        print("Invalid")
-    elif (at ==-1):
-        print("Invalid")
-    else:
-        print("Valid")
+
+    #'@' should come before last '.'
+    if at<1 or dot<at+2 or dot == len(address) -1:
+        return False
+
+    return True
 
 print("This program will decide if your input is a valid email address")
 while(True):
     print("A valid email address needs an '@' symbol and a '.'")
-    x = input("Input your email address: ")
+    x = input("Input your email address(type 'exit' to quit): ")
     
-    addressVal(x)
+    if x.lower() == "exit":
+        break
+    
+    if addressVal(x):
+        print("Valid")
+    else:
+        print("Invalid")
