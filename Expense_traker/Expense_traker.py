b14c26f920df588b3ce584e1170b35d7a2a389cd
Keyur-0 <keyurkhnat001@gmail.com>
2025-08-31 21:58:10 +0530

    adding a function and a test

--- Expense_traker/Expense_traker.py
+++ Expense_traker/Expense_traker.py
@@ -1,2 +1,17 @@
 class ExpenseTracker:
-    def
+    def __init__(self):
+        self.expenses = {}
+        self.categories = {}
+
+    def add_expense(self, date, amount, category):
+        if category not in self.expenses:
+            self.expenses[category] = []
+        self.expenses[category].append((date, amount))
+
+#test the function
+tracker = ExpenseTracker()
+tracker.add_expense("2025-08-31", 200, "Food")
+tracker.add_expense("2025-08-30", 100, "Food")
+tracker.add_expense("2025-08-29", 500, "Transport")
+
+print(f"{tracker.expenses}")
